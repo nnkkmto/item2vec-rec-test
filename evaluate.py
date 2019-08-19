85c81bdb14711a2bef9d05004f8ec06c792b1dd2
komoto-naoki <komoto_naoki@microad.co.jp>
2019-08-19 13:17:53 +0900

    modify

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,6 @@
 
+import os
+
 import pandas as pd
 from gensim.models import word2vec
 
@@ -10,16 +12,20 @@ def prepare_data():
     columns = ['order_id', 'order_number', 'add_to_cart_order', 'product_id']
 
     df = data_handler.load_data(filepath, columns)
+    item_list = df.product_id.unique()
 
-    df = df.sort_values(by=['order_id', 'order_number', 'add_to_cart_order'])
-    df = df.rename(columns={'product_id':'label'})
-    df['product_id'] = df['label'].shift(1)
-    df = df[df.add_to_cart_order != '1']
-    df_label = df[['product_id', 'label']]
+    df_label_mrr = df.sort_values(by=['order_id', 'order_number', 'add_to_cart_order'])
+    df_label_mrr = df_label_mrr.rename(columns={'product_id':'label'})
+    df_label_mrr['product_id'] = df_label_mrr['label'].shift(1)
+    df_label_mrr = df_label_mrr[df_label_mrr.add_to_cart_order != '1']
+    df_label_mrr = df_label_mrr[['product_id', 'label']]
 
-    item_list = df_label.product_id.unique()
+    df_label_map = df.rename(columns={'product_id': 'label'})
+    df_label_map = df_label_map[['order_id', 'label']]
+    df_label_map = pd.merge(df[['order_id', 'product_id']], df_label_map, on='order_id', how='left')
+    df_label_map = df_label_map[df_label_map.product_id != df_label_map.label]
 
-    return df_label, item_list
+    return df_label_mrr, df_label_map, item_list
 
 
 def predict(model, item_list):
@@ -35,7 +41,7 @@ def predict(model, item_list):
         df_pred['product_id'] = item
         df_pred['rank'] = df_pred['distance'].rank(ascending=False)
 
-        df = pd.concat([df, df_pred])
+        df = pd.concat([df, df_pred], sort=True)
 
     return df
 
@@ -47,25 +53,50 @@ def evaluate_mrr(df_label, df_pred):
     n = len(df)
 
     df_calc = df.dropna()
-    print(df_calc)
-    df_calc['calc'] = 1 / df.rank
-    print(df_calc)
+    df_calc['calc'] = 1 / df_calc['rank'].astype(int)
     rr = df_calc.calc.sum()
+    mrr = rr / n
+
     print(rr)
     print(n)
-    print(rr / n)
+    print(mrr)
 
 
+def evaluate_map(df_label, df_pred):
+
+    # 最小単位はorder_id*product_idなので、修正
+    df = pd.merge(df_label, df_pred, on=['product_id', 'label'], how='left')
+
+    n = len(df[['order_id', 'product_id']].drop_duplicates())
+
+
+    df = df.dropna()
+    print(df)
+    df['correct'] = df.groupby(by=['order_id', 'product_id'])['rank'].rank(ascending=True)
+    print(df)
+    df['ap'] = df.correct / df['rank'].astype(int)
+    print(df)
+    ap = df.ap.sum()
+
+    map_metr = ap / n
+    print(map_metr)
 
 
 def main():
+    df_label_mrr, df_label_map, item_list = prepare_data()
 
-    model = word2vec.Word2Vec.load('model/i2v.model')
+    for model_name in ['simple_w2v', 'max_window_w2v', 'i2v']:
 
-    df_label, item_list = prepare_data()
-    df_pred = predict(model, item_list)
+        model_path = os.path.join('model/', model_name+'.model')
+        model = word2vec.Word2Vec.load(model_path)
 
-    evaluate_mrr(df_label, df_pred)
+        df_pred = predict(model, item_list)
+
+        print(f'MAP: {model_name}\n')
+        evaluate_map(df_label_map, df_pred)
+
+        print(f'MRR: {model_name}\n')
+        # evaluate_mrr(df_label_mrr, df_pred)
 
 
 if __name__ == '__main__':
